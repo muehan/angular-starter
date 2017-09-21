c30b518509041cbc68eddf92e3fe52789ff1947b
André <tdc@gmx.ch>
2017-09-21 19:07:53 +0200

    api generator

---
+++ src/api/api/api.ts
@@ -0,0 +1,7 @@
+export * from './CustomerApi';
+import { CustomerApi } from './CustomerApi';
+export * from './ItemApi';
+import { ItemApi } from './ItemApi';
+export * from './OrderApi';
+import { OrderApi } from './OrderApi';
+export const APIS = [CustomerApi, ItemApi, OrderApi];
