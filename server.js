a3455a267b7bbbcdf9ae02a238c9bf1b491b64f0
André <tdc@gmx.ch>
2017-09-25 19:51:19 +0200

    add extress for heroku browsing

---
+++ server.js
@@ -0,0 +1,15 @@
+/*
+    This file is here to make the dist folder available in heroku deployment
+*/
+
+var path = require('path');
+var express = require('express');
+
+var app = express();
+
+app.use(express.static(path.join(__dirname, 'dist')));
+app.set('port', process.env.PORT || 8080);
+
+var server = app.listen(app.get('port'), function() {
+    console.log('listening on port ', server.address().port);
+});
