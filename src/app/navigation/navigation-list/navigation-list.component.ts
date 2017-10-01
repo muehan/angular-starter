f5dbc34b42576ee204f19101da1314c8185c5fcb
André <tdc@gmx.ch>
2017-10-01 16:00:03 +0200

    show list component if items route is active

--- src/app/navigation/navigation-list/navigation-list.component.ts
+++ src/app/navigation/navigation-list/navigation-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router, NavigationEnd } from '@angular/router';
 import { AppState } from './app.service';
 import { MaterialModule } from '@angular/material';
   
@@ -11,36 +11,36 @@ import { MaterialModule } from '@angular/material';
 
 export class NavigationListComponent implements OnInit {
 
-  public localState: any;
+  private localState: any;
+  private currentRoute: string = null;
+
   constructor(
-    public route: ActivatedRoute
+    private route: ActivatedRoute,
+    private router: Router,
   ) {}
 
   public ngOnInit() {
-    this.route
-      .data
-      .subscribe((data: any) => {
-        /**
-         * Your resolved data from route.
-         */
+    this.route.data.subscribe((data: any) => {
         this.localState = data.yourData;
       });
 
-    console.log('hello `Navigation-list` component');
-    /**
-     * static data that is bundled
-     * var mockData = require('assets/mock-data/mock-data.json');
-     * console.log('mockData', mockData);
-     * if you're working with mock data you can also use http.get('assets/mock-data/mock-data.json')
-     */
+      this.router.events.subscribe(event => {
+        if (event instanceof NavigationEnd ) {
+          console.log("current url",event.url);
+          this.currentRoute = event.url;
+        }
+      })
+
     this.asyncDataWithWebpack();
   }
+
+  public listModuleVisiable(){
+    if(this.currentRoute != null) {
+      return this.currentRoute.includes('items');
+    }
+  }
+
   private asyncDataWithWebpack() {
-    /**
-     * you can also async load mock data with 'es6-promise-loader'
-     * you would do this if you don't want the mock-data bundled
-     * remember that 'es6-promise-loader' is a promise
-     */
     setTimeout(() => {
 
       System.import('../../../assets/mock-data/mock-data.json')
