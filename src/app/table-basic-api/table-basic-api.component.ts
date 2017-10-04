d108ac75b0a13ca18be8849f1c0bb73fd9454ce1
Andre Muehle <andre.muehle@migrosluzern.ch>
2017-10-04 15:03:09 +0200

    implemented delete with complicated behaviorSubject

--- src/app/table-basic-api/table-basic-api.component.ts
+++ src/app/table-basic-api/table-basic-api.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { DataSource } from '@angular/cdk/collections';
 import { Observable } from 'rxjs/Observable';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import { CustomerApi } from './../../api';
 import { Customer } from './../../api';
 import 'rxjs/add/observable/of';
@@ -16,29 +17,42 @@ import 'rxjs/add/observable/of';
 
 export class TableBasicApiComponent {
 
-  constructor(private customerApi:CustomerApi){
-    
-  }
+  constructor(private customerApi: CustomerApi) { }
+
+  customers = <BehaviorSubject<Customer[]>>new BehaviorSubject([]);
+  customerStore = { customers: [] };
 
   displayedColumns = ['guid', 'name', 'prename', 'street', 'streetnumber', 'zipcode', 'city', 'func'];
-  dataSource = new ExampleDataSource(this.customerApi);
+  dataSource = new ExampleDataSource(this.customers);
+
+  ngOnInit() {
+    this.customerApi.apiCustomerGet().subscribe(
+      response => {
+        this.customerStore.customers = response;
+        this.customers.next(Object.assign({}, this.customerStore).customers);
+      });
+  }
 
   delete(id) {
     console.log('delete:' + id);
-    this.customerApi.apiCustomerByIdDelete(id).subscribe(response => console.log(response));
+    this.customerApi.apiCustomerByIdDelete(id).subscribe(response => {
+      this.customers.next(this.customers.getValue().filter(function (item) {
+        return item.guid != id;
+      }));
+    });
   }
 }
 
 export class ExampleDataSource extends DataSource<any> {
 
-  constructor(private customerApi:CustomerApi){
+  constructor(private customers: BehaviorSubject<Customer[]>) {
     super();
   }
 
   /** Connect function called by the table to retrieve one stream containing the data to render. */
   connect(): Observable<Customer[]> {
-    return this.customerApi.apiCustomerGet()
+    return this.customers.asObservable();
   }
 
-  disconnect() {}
+  disconnect() { }
 }
