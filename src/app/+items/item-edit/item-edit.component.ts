d1c3c9bff56c145aad4b796fddf31a79a47fdae0
Andre Muehle <andre.muehle@migrosluzern.ch>
2017-10-05 15:51:40 +0200

    with loading and change detecting, somehow ugly...

--- src/app/+items/item-edit/item-edit.component.ts
+++ src/app/+items/item-edit/item-edit.component.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { Router, ActivatedRoute, Params } from '@angular/router';
-
 import { ItemApi, Item, ItemEditCommand } from './../../../api';
 
 @Component({
@@ -11,19 +10,24 @@ import { ItemApi, Item, ItemEditCommand } from './../../../api';
 export class ItemEditComponent implements OnInit {
 
   private item: Item = {};
+  private loading: boolean = true;
 
   constructor(
     private activatedRoute: ActivatedRoute,
     private router: Router,
     private itemApi: ItemApi,
+    private ref:ChangeDetectorRef,
   ) { }
 
   ngOnInit() {
 
     this.activatedRoute.params.subscribe((params: Params) => {
       let itemId = params['id'];
-      
-      this.itemApi.apiItemIdGet(itemId).subscribe(data => this.item = data.items[0]);
+      this.itemApi.apiItemIdGet(itemId).subscribe(data => {
+        this.item = data.items[0];
+        this.loading = false;
+        this.ref.detectChanges();
+      });
     })
   }
 
