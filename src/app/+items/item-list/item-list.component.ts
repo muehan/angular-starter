9adba8bb1873737a0bcf04d7b5b57f751d74826d
Andre Muehle <andre.muehle@migrosluzern.ch>
2017-10-06 08:02:19 +0200

    add loading indicator for items

--- src/app/+items/item-list/item-list.component.ts
+++ src/app/+items/item-list/item-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { DataSource } from '@angular/cdk/collections';
 import { Observable } from 'rxjs/Observable';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
@@ -13,23 +13,27 @@ import { ItemApi, Item, ItemCreateCommand } from './../../../api';
 
 export class ItemListComponent implements OnInit {
 
-  constructor(private itemApi: ItemApi) {
-
-  }
+  constructor(
+    private itemApi: ItemApi,
+    private ref: ChangeDetectorRef,
+  ) {  }
 
   displayedColumns = ['number', 'vendorNumber', 'name', 'price', 'func'];
 
   items = <BehaviorSubject<Item[]>>new BehaviorSubject([]);
   dataSource = new ItemDataSource(this.items);
   createModel = new ItemCreateModel();
+  private loading: boolean = true;
 
   ngOnInit() {
     this.itemApi.apiItemGet().map(data => data.items).subscribe(res => {
       this.items.next(res);
+      this.loading = false;
+      this.ref.detectChanges();
     });
   }
 
-  createItem(){
+  createItem() {
     this.itemApi.apiItemPost(this.createModel).subscribe(response => {
       this.items.next(this.items.getValue().concat(response.item));
     })
