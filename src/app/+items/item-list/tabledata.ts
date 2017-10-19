651253b015db2d844cd9ff7d8cb703543553af73
Andre Muehle <andre.muehle@migrosluzern.ch>
2017-10-19 15:10:42 +0200

    removed comments, made usefuller comments, removed unnessessary code

--- src/app/+items/item-list/tabledata.ts
+++ src/app/+items/item-list/tabledata.ts
@@ -27,26 +27,18 @@ export class DataTable<T extends Iitem> {
                         var value = filterItem.nativeElement.value;
                         var column = filterItem.nativeElement.placeholder;
 
-                        console.log("keyEvent '" + value + "' from: " + column);
-
                         // all chars removed, remove filter from columnFilterArray
                         if (value === '') {
                             if (this.dataSource.filters.some(x => x.column === column)) {
-                                console.log('remove colum from current filter: ' + column);
                                 this.dataSource.filters = this.dataSource.filters.filter(x => x.column !== column);
                             }
                         } else {
                             if (!this.dataSource.filters.some(x => x.column === column)) {
-                                // console.log("new column filter added");
                                 this.dataSource.filters = this.dataSource.filters.concat({column: column, value: value});
                             } else {
-                                // console.log("add new value to existing filter: " + column + " - " + value);
-                                var existingFilter = this.dataSource.filters.find(x => x.column === column);
-
+                                // sadly a steam... you cant manipulate data inside the getter
                                 this.dataSource.filters = this.dataSource.filters.filter(x => x.column !== column);
                                 this.dataSource.filters = this.dataSource.filters.concat({column: column, value: value});
-
-                                var existingFilter = this.dataSource.filters.find(x => x.column === column);
                             }
                         }
                     })
@@ -83,14 +75,6 @@ class ItemDataSource<T> extends DataSource<any> {
     get filters(): ISearchFilter[] { return this._filters.value }
     set filters(filter: ISearchFilter[]) { this._filters.next(filter); }
 
-    // _filterChange = new BehaviorSubject('');
-    // get filter(): string { return this._filterChange.value; }
-    // set filter(filter: string) { this._filterChange.next(filter); }
-
-    // _columnfilter: string[] = [];
-    // get columnfilter(): string[] { return this._columnfilter; }
-    // set columnfilter(filter: string[]) { this._columnfilter = filter; }
-
     constructor(
         private items: BehaviorSubject<T[]>,
         private sort: MdSort
@@ -113,27 +97,12 @@ class ItemDataSource<T> extends DataSource<any> {
                 var show = true;
 
                 this.filters.forEach(filterElement => {
-                    console.log(item[filterElement.column].toString().toLowerCase());
-                    console.log(filterElement.value.toLowerCase());
-
                     if(item[filterElement.column].toString().toLowerCase().indexOf(filterElement.value.toLowerCase()) == -1){
                         show = false;
                     }   
                 })
-                console.log(show);
                 return show;
             });
-
-            // var props = Object.getOwnPropertyNames(item);
-            
-            // let searchStr = "";
-            // props.forEach(element => {
-            //     searchStr = searchStr + item[element]
-            // });
-
-            // searchStr = searchStr.toLowerCase();
-
-            // return searchStr.indexOf(this.filter.toLowerCase()) != -1;
         });
 
     }
@@ -141,7 +110,6 @@ class ItemDataSource<T> extends DataSource<any> {
     disconnect() { }
 
     getSortedData(): T[] {
-        console.log('get sorted data');
         const data: T[] = this.items.value.slice();
         var props = Object.getOwnPropertyNames(data[0]);
         if (!this.sort.active || this.sort.direction == '') { return data; }
